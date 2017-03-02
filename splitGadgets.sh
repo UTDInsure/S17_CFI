f4243e2aa1515f49155bdc2a6e83721a7b3a9e3b
Joel Seida <jseida2@gmail.com>
2017-03-02 15:08:19 -0800

    Initial commit

---
+++ splitGadgets.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -gt 0 ]; then
+  if [ $# -gt 1 ]; then
+    echo "ERROR: Too many arguments"
+    exit 1
+  else
+    echo "Working on :" $1
+  fi
+else
+  echo "ERROR: Did not include a folder to sort"
+  exit 1
+fi
+
+[ -d splittemp ] || mkdir splittemp
+
+cd splittemp
+
+for filename in ../$1/*.txt
+do
+  csplit -n5 -s --prefix=${filename##*/} $filename "/========/+1" "{*}"
+done
+
+
+
+
+# rm -rf splittemp
