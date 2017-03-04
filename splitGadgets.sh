32be921cfb3ce2eb531a0ba90a409d15a1c02131
Jeremiah Shipman <Jeremiah@cometnet-10-21-64-9.utdallas.edu>
2017-03-03 20:16:52 -0600

    Finished easy/hard gadget sorter

--- splitGadgets.sh
+++ splitGadgets.sh
@@ -13,17 +13,39 @@ else
 fi
 
 [ -d splittemp ] || mkdir splittemp
-
+[ -d hard ] || mkdir hard
+[ -d easy ] || mkdir easy
 cd splittemp
 
 for filename in ../$1/*.txt
 do
-  csplit -n5 -s --prefix=${filename##*/} $filename "/========/+1" "{*}"
+  csplit -n5 -s --prefix=${filename##*/} $filename "/========/+1" "{*}" #change to gcsplit on MacOS
 done
 
-for filename in ./*
+
+for file in splittemp/*
 do
-    # analyze the gadget
+	hard=false
+	IFS=$'\n' read -d '' -r -a lines < $file  #make an array called lines from the file 
+	if [ ${#lines[@]} -lt 4 ]; then #make sure our gadget is big enough to test
+		continue
+	fi
+	for i in $(seq 1 $((${#lines[@]} - 3 )));  #skips the first and last command of every gadget
+	do
+		tokens=( ${lines[$i]} )  #tokenize the line
+		case "${tokens[1]}" in  #test the command part of the line
+  			J*|CALL)
+    			hard=true
+    			;;
+		esac
+	done
+
+	if $hard; then
+		cp $file hard
+	else
+		cp $file easy
+	fi
+
 done
 
 
